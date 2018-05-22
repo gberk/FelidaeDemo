b436bdad9ccc67bf3dde439245eddee8ef2d5c68
Gavin Berkowitz <gavin.berkowitz.bu@gmail.com>
2018-05-21 21:47:39 -0400

    Log intent when InactiveIntentError happens

--- Errors/index.js
+++ Errors/index.js
@@ -3,6 +3,7 @@ var Ayva = require('ayva')
 var StateManager = require('../DataStores/StateProvider')
 
 var InactiveIntentErrorHandler = function(Context){
+    console.log("Intent matched: " + Context.intentName)
     StateManager.getState(Context).then(function(state) {
         Context.assistant.say("This command is currently not available.").finish()
     })
