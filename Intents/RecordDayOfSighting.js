8c7481ed1572ca5276aed28a45979031ff64c7e7
Gavin Berkowitz <gavin.berkowitz.bu@gmail.com>
2018-05-28 15:30:24 -0400

    Package version update

--- Intents/RecordDayOfSighting.js
+++ Intents/RecordDayOfSighting.js
@@ -11,7 +11,6 @@ var RecordDayOfSighting = function(Context){
     var dateMatchedString = Context.args.dateOfSighting.match(dateTimeRegex)
     var dateMatch = dateMatchedString[1]
     var timeMatch = dateMatchedString[6]
-    console.log(dateMatch, timeMatch)
 
     //DATE PREPROCESSING
     //Is the date in the future? Rewind to this year
