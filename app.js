93e4cf943eecc16496f114701fe209a469af6169
kevin.lee@refreshlabs.co <kevin.lee@refreshlabs.co>
2018-06-16 10:06:21 -0700

    added sendgrid email abilities

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
+var axios = require('axios');
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -46,6 +48,47 @@ app.get('/privacyPolicy', function(req, res) {
 
 app.get('/notification', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/notification.html'))
+})
+
+app.post('/sendEmail', function (req, res) {
+    
+    let email = req.body.emailAddress
+    let sendGridAuth = "Bearer " + process.env.SENDGRID_API_KEY
+    let sendGridRequest = {
+        "personalizations": [{
+            "to": [{
+                "email": email
+            }],
+            "subject": "Hello, World!"
+        }],
+        "from": {
+            "email": "founders@example.com"
+        },
+        "content": [{
+            "type": "text/plain",
+            "value": "Hello, World!"
+        }]
+    }
+
+    // save email address to db
+
+    // send email to felidae team
+    axios({
+        method:'post',
+        headers: {
+            Authorization: sendGridAuth
+        },
+        url: 'https://api.sendgrid.com/v3/mail/send',
+        data: sendGridRequest
+    }).then((response) => {
+        let result = {
+            status: response.status,
+            statusText: response.statusText
+        }
+        res.send(result)
+    }).catch((error) => {
+        res.send(error)
+    })
 })
 
 server.listen(process.env.PORT || 8080, function() {
