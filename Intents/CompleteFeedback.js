cbfbf1f6612c192e0a116cfc2e5128ec9ca74842
Gavin Berkowitz <gavin.berkowitz.bu@gmail.com>
2018-06-16 22:28:11 -0400

    ReportId added to feedback

--- Intents/CompleteFeedback.js
+++ Intents/CompleteFeedback.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 const ConversationLog = require('../DataStores/ConversationLog')
-
+var UserStore = require("../DataStores/UserStore")
 var CompleteFeedback = function(Context){    
     ConversationLog.log(Context)
     let platform = "Unknown"
@@ -15,17 +15,24 @@ var CompleteFeedback = function(Context){
     if (Context.deviceProfile.platform === "alexa") {
         platform = "Alexa"
     }
-    let feedback = platform + " - " + isMobile + " || " + Context.args.feedback;
-    axios({
-        method: 'post',
-        headers:{"Content-type":"application/json"},
-        data: {text: feedback},
-        url: process.env.FEEDBACK_RIVER_APP
-    }).then((response) => {
-        console.log("Feedback sent to Slack successfully.")
-    }).catch((error) => {
-        console.log("Feedback error: " + error)
+
+    let feedback;
+    UserStore.get(Context).then((userData) => 
+    {
+        var conversationId = userData.conversationId;
+        feedback =  conversationId + " || " + platform + " - " + isMobile + " || " + Context.args.feedback;
+        axios({
+            method: 'post',
+            headers:{"Content-type":"application/json"},
+            data: {text: feedback},
+            url: process.env.FEEDBACK_RIVER_APP
+        }).then((response) => {
+            console.log("Feedback sent to Slack successfully.")
+        }).catch((error) => {
+            console.log("Feedback error: " + error)
+        })
     })
+
     Context.assistant
         .say("Thank you. Your feedback has been sent to the team.")
         .finish({"exit":true});
